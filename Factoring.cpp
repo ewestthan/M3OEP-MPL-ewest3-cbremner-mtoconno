b409b6abc7383d587ca32da3fd7633ed83403669
cambremnerr <cajbremner@gmail.com>
2021-04-07 20:27:28 -0400

    factoring method works

--- Factoring.cpp
+++ Factoring.cpp
@@ -1,10 +1,25 @@
 #include <string>
+#include<iostream>
 using namespace std;
 
-string factor();
+string factor(string numStr);
 
-string factor(string input){
-    //create factor
+/*
+int main() {
+    int i = 543;
+    string number = to_string(i);
+    string s = factor(number);
+    cout << s << endl;
+}
+*/
 
-    return factor;
+string factor(string numStr){
+    int num = stoi(numStr);
+    string factors;
+    for(int i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            factors.append(to_string(i) + " ");
+        }
+    }
+    return factors;
 };
