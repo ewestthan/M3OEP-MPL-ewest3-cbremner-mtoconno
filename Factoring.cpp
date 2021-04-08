e9b0cda77ce82f7125a06cb4c4561ec08011093a
cambremnerr <cajbremner@gmail.com>
2021-04-08 00:10:34 -0400

    updated cpp file to read from console

--- Factoring.cpp
+++ Factoring.cpp
@@ -2,10 +2,18 @@
 #include<iostream>
 using namespace std;
 
-string factor(string numStr);
+string factor(int num);
 
-string factor(string numStr){
+
+int main(int argc, char** argv) {
+    string numStr = argv[1];
     int num = stoi(numStr);
+    string factors_out = factor(num);
+    cout << factors_out << endl;
+}
+
+
+string factor(int num){
     string factors;
     for(int i = 1; i <= num; i++) {
         if (num % i == 0) {
