959fef3d90dc1f977a37155819b5b8f8a6af2c9f
ewestthan <49725521+ewestthan@users.noreply.github.com>
2021-04-08 20:53:37 -0600

    ignore

--- factorGUI.java
+++ factorGUI.java
@@ -2,11 +2,11 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.io.*;
 
-public class Factoring extends JFrame implements ActionListener {
+public class factorGUI extends JFrame implements ActionListener {
     JLabel lb1,lb2;
     JTextField ta;
     JButton b;
-    Factoring(){
+    factorGUI(){
         super("Factoring Tool");
         lb1=new JLabel("Enter a big number with at least 7 digits:");
         lb1.setBounds(50,50,300,20);
@@ -14,12 +14,15 @@ public class Factoring extends JFrame implements ActionListener {
         ta=new JTextField();
         ta.setBounds(50,100,100,20);
 
+        lb2=new JLabel("Factors:");
+        lb2.setBounds(50, 200, 300, 20);
+
         b=new JButton("Factor");
         b.setBounds(50,320, 80,30);//x,y,w,h  
         b.addActionListener(this);
 
 
-        add(lb1);add(ta);add(b);
+        add(lb1);add(lb2);add(ta);add(b);
 
         setSize(400,400);
         setLayout(null);//using no layout manager  
@@ -27,15 +30,12 @@ public class Factoring extends JFrame implements ActionListener {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-    static{
-        System.loadLibrary("Factoring");
-    }
 
     public void actionPerformed(ActionEvent e) {
         try {
             if (e.getSource() == b) {
                 String text = ta.getText();
-                lb1.setText("Factors: " + text);
+                lb2.setText("Factors: " + text);
                 writeNumberToFile();
             }
         } catch (IOException ioe) {
@@ -51,9 +51,6 @@ public class Factoring extends JFrame implements ActionListener {
     }
 
     public static void main(String[] args){
-
-        new Factoring();
-
         try {
            ProcessBuilder builder = new ProcessBuilder("Factoring.exe");
            Process process = builder.start();
@@ -68,5 +65,7 @@ public class Factoring extends JFrame implements ActionListener {
         } catch(Exception e) {
             e.printStackTrace();
         }
+        
+        new factorGUI();
     }
 }
